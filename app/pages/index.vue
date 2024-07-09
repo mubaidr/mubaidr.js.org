567a7bf02b03e829a828331f53bc1c0ac1c715ff
Muhammad Ubaid Raza <mubaidr@gmail.com>
2024-07-09 09:22:54 +0500

    set nuxt v4

---
+++ app/pages/index.vue
@@ -0,0 +1,8 @@
+<script lang="ts" setup></script>
+
+<template>
+  Testing...
+  <NuxtWelcome />
+</template>
+
+<style lang="scss" scoped></style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,9 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   debug: true,
+  future: {
+    compatibilityVersion: 4,
+  },
   modules: [
     '@nuxt/content',
     // '@nuxt/eslint',
@@ -47,5 +50,10 @@ export default defineNuxtConfig({
   // contentAssets: {},
   studio: {
     enabled: true,
+  },
+  experimental: {
+    typedPages: true,
+    sharedPrerenderData: true,
+    writeEarlyHints: true,
   },
 })
