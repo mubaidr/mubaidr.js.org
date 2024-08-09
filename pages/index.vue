18f5c14d2614a25aba7063f1804b113e63e730f0
Muhammad Ubaid Raza <mubaidr@gmail.com>
2024-08-09 10:28:52 +0500

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -3,22 +3,22 @@
 <template>
   <main>
     <div>
-      <h2 class="text-3xl font-extrabold mb-2">Hey there!</h2>
+      <h2 class="text-3xl font-extrabold">Hey there!</h2>
       <ContentDoc path="/about" />
     </div>
 
     <div class="mt-12">
-      <h2 class="text-3xl font-extrabold mb-2">Tech Stack & Tools I Use</h2>
+      <h2 class="text-3xl font-extrabold">Tech Stack & Tools I Use</h2>
       <TechStack />
     </div>
 
     <div class="mt-12">
-      <h2 class="text-3xl font-extrabold mb-2">What I Do Well</h2>
+      <h2 class="text-3xl font-extrabold">What I Do Well</h2>
       <ContentDoc path="/what-i-do" />
     </div>
 
     <div class="mt-12">
-      <h2 class="text-3xl font-extrabold mb-2">Get In Touch</h2>
+      <h2 class="text-3xl font-extrabold">Get In Touch</h2>
       <ContentDoc path="/get-in-touch" />
     </div>
   </main>
