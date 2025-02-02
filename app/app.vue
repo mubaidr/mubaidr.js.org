4f45507301731b451d2c8e0fb24ae7a40dd1cc95
Muhammad Ubaid Raza <mubaidr@gmail.com>
2025-02-02 14:35:57 +0500

    Update app.vue

--- app/app.vue
+++ app/app.vue
@@ -23,6 +23,10 @@ useHead({
       name: "google-site-verification",
       content: "HCL7-zzyR002AD-QxBUEhq9uwSKx2KWCsamU0ANbBW8",
     },
+    {
+      name: "msvalidate.01",
+      content: "8E520DCC754D9DE129C9B0DAC9334070",
+    },
   ],
 })
 </script>
