3c6c65cd64bcf7ce04dee8c820d7e77aa5732b78
Muhammad Ubaid Raza <mubaidr@gmail.com>
2025-07-04 15:09:13 +0500

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -28,14 +28,17 @@ export default defineNuxtConfig({
           },
         },
         highlight: {
+          // Theme used in all color schemes.
+          theme: 'github-light',
+          // OR
           theme: {
             // Default theme (same as single string)
-            default: "github-light",
+            default: 'github-light',
             // Theme used if `html.dark`
-            dark: "github-dark",
+            dark: 'github-dark',
             // Theme used if `html.sepia`
-            sepia: "monokai",
-          },
+            sepia: 'monokai'
+          }
         },
         toc: {
           depth: 2,
